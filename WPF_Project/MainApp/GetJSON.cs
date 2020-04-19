8352eb9378c9b5d86e9db2e148eebd54de6e6227
NickSkz <skubiszmikolaj450@gmail.com>
2020-04-19 17:43:06 +0200

    WPF Folder added.

---
+++ WPF_Project/MainApp/GetJSON.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace JsonProcessing
+{
+    class GetJSON
+    {
+        public static String getData()
+        {
+            var client = new WebClient();
+            return client.DownloadString("https://api.thevirustracker.com/free-api?countryTotal=PL");
+        }
+    }
+}
