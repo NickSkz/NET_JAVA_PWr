b8c7c6030dcab41f5545c35e5c07f6ce58368a07
Przemo <62259426+Przelodziej@users.noreply.github.com>
2020-04-21 02:33:55 +0200

    Add files via upload
    Okna

---
+++ WPF_Project/WPF_Project/MainApp/GetJSON.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Text;
+
+using LoggerSpace;
+
+/*Nuget Packages*/
+using Newtonsoft.Json;
+
+namespace JsonProcessing
+{
+    class GetJSON
+    {
+        public static String GetData(string desiredCountry)
+        {
+            //use WebClient object (recieve data from URL),
+            using (var client = new WebClient())
+            {
+                //Get raw JSON, Deserialize it into object
+                String trueJson = client.DownloadString(desiredCountry);
+                JsonObject countryInfo = JsonConvert.DeserializeObject<JsonObject>(trueJson);
+                Logger.log.Info("");
+                return (Convert.ToString(countryInfo.countrydata[0].total_cases));
+
+            }
+        }
+    }
+}
