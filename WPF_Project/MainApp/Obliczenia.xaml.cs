9ea8170ca3340b0364101ac02d9444864fef5d40
Przelodziej <235613@student.pwr.edu.pl>
2020-04-22 18:45:17 +0200

    Dalsza czesc GUI

--- WPF_Project/MainApp/Obliczenia.xaml.cs
+++ WPF_Project/MainApp/Obliczenia.xaml.cs
@@ -9,6 +9,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using JsonProcessing;
+using System.Text.RegularExpressions;
 
 namespace MainApp
 {
@@ -17,19 +19,46 @@ namespace MainApp
     /// </summary>
     public partial class Obliczenia : Window
     {
+        private String Country;
         public Obliczenia()
         {
             InitializeComponent();
         }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+
+        private void btn_count(object sender, RoutedEventArgs e)
         {
-            
+            int NumOfP= Int32.Parse(perDay.Text); // liczba ludzi spotykanych jednego dnia
+            int PerInContry;
+            JsonObject CountryData = GetJSON.GetData(Country);
+            int TotalCases= CountryData.countrydata[0].total_cases;
+            Chance.Content = TotalCases/NumOfP;
+
+        }
+        private static readonly Regex _regex = new Regex("[^0-9.-]+"); //regex that matches disallowed text
+        private static bool IsTextAllowed(string text)
+        {
+            return !_regex.IsMatch(text);
+        }
+        private void PrevText(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed(e.Text);
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void lb_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            List<String> vec = new List<String>{
+                CountryJsonLink.addrPL,
+                CountryJsonLink.addrDE,
+                CountryJsonLink.addrCZ,
+                CountryJsonLink.addrSK,
+                CountryJsonLink.addrUA,
+                CountryJsonLink.addrBY,
+                CountryJsonLink.addrLT,
+                CountryJsonLink.addrRU,
+            };
 
+            Country=vec[lb.SelectedIndex];
         }
     }
    
