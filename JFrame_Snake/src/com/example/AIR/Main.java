207267e206908348925b2b78e17483218881026b
Przelodziej <235613@student.pwr.edu.pl>
2020-05-26 00:33:35 +0200

    Teraz robi sie wykres z bazy jak klikniemy. Choc widac jak widac. Funkcjonalnosc jest XD

--- JFrame_Snake/src/com/example/AIR/Main.java
+++ JFrame_Snake/src/com/example/AIR/Main.java
@@ -1,8 +1,20 @@
 package com.example.AIR;
 
+import javax.swing.*;
+import java.awt.*;
+import java.security.KeyStore;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        JFrame obj = new JFrame();
+        Plansza plansza = new Plansza();
+
+        obj.setBounds(10,10,600,600);
+        obj.setBackground(Color.BLACK);
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(plansza);
     }
 }
---
+++ JFrame_Snake/src/com/example/AIR/Plansza.java
@@ -0,0 +1,12 @@
+package com.example.AIR;
+
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Color;
+
+public class Plansza extends JPanel {
+    public Plansza()
+    {
+
+    }
+}
